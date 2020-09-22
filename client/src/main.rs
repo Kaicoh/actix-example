4683c256a3e99e26d2961e9e2476bdb2ec6c4832
Kanji Tanaka <ktanaka@ichain.co.jp>
2020-09-22 15:07:55 +0900

    Add error handling

--- client/src/main.rs
+++ client/src/main.rs
@@ -8,5 +8,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     println!("body = {:?}", body);
+
+    let result = reqwest::get("http://localhost:8000/error").await?;
+
+    println!("result.status() = {:?}", result.status());
+
+    let body = result.text().await?;
+    println!("body = {:?}", body);
+
     Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,10 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+extern crate derive_more;
+
+use actix_web::{
+    dev::HttpResponseBuilder, error, get, http::header, http::StatusCode,
+    App, HttpResponse, HttpServer, Responder,
+};
+use derive_more::{Display, Error};
 use models::Hello;
 
 #[get("/")]
@@ -8,13 +14,39 @@ async fn hello() -> impl Responder {
     })
 }
 
+#[get("/error")]
+async fn hello_err() -> Result<&'static str, MyError> {
+    Err(MyError::BadRequest)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
+            .service(hello_err)
     })
     .bind("0.0.0.0:8000")?
     .run()
     .await
+}
+
+#[derive(Debug, Display, Error)]
+enum MyError {
+    #[display(fmt = "bad request")]
+    BadRequest,
+}
+
+impl error::ResponseError for MyError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponseBuilder::new(self.status_code())
+            .set_header(header::CONTENT_TYPE, "text/plain; charset=utf-8")
+            .body(self.to_string())
+    }
+
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            MyError::BadRequest => StatusCode::BAD_REQUEST,
+        }
+    }
 }
